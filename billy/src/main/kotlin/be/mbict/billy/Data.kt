43d31198e607854885da28c7b35d37d42dc5f4e0
Marc Bogaerts <marc.je.bogaerts@telenet.be>
2024-10-13 18:36:59 +0200

    Lees data uit Artemis en sla op in H2 database

--- billy/src/main/kotlin/be/mbict/billy/Data.kt
+++ billy/src/main/kotlin/be/mbict/billy/Data.kt
@@ -3,12 +3,12 @@ package be.mbict.billy
 import io.github.oshai.kotlinlogging.KotlinLogging
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
+import jakarta.transaction.Transactional
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.jms.annotation.JmsListener
+import org.springframework.stereotype.Component
 import org.springframework.stereotype.Repository
-import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -16,20 +16,12 @@ private val log = KotlinLogging.logger { }
 
 @RestController
 @RequestMapping("/data")
-class Controller(
-    private val repo: DataRepository
-) {
+class Controller(private val repo: DataRepository) {
 
     @GetMapping
     fun allData(): Iterable<Data> {
         log.info { "Retrieve all data" }
         return repo.findAll()
-    }
-
-    @PostMapping
-    fun add(@RequestBody data: Data) {
-        log.info { "Add $data" }
-        repo.save(data)
     }
 }
 
@@ -41,3 +33,14 @@ class Data(
 
 @Repository
 interface DataRepository : JpaRepository<Data, Int>
+
+@Component
+class JmsListener(private val repo: DataRepository) {
+
+    @Transactional
+    @JmsListener(destination = "data-stream")
+    fun addData(data: Data) {
+        log.info { "Received $data" }
+        repo.save(data)
+    }
+}
--- billy/src/main/kotlin/be/mbict/billy/JmsConfig.kt
+++ billy/src/main/kotlin/be/mbict/billy/JmsConfig.kt
@@ -1,4 +1,4 @@
-package be.mbict.carlotta
+package be.mbict.billy
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
